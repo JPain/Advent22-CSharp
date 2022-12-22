f1a315e1fc3b63ba7bc2a2a6074de62007fa72b3
James Pain <james@thinkbit.uk>
2022-12-22 15:08:19 +0000

    Day 01 Part 2 Done

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -9,9 +9,9 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    private string Part1()
+    private string CountTotalCalories(int totalsToCount)
     {
-        int maxCalories = 0;
+        List<int> caloriesList = new List<int>();
         foreach (string block in _input.Split($"{Environment.NewLine}{Environment.NewLine}"))
         {
             int blockCalories = 0;
@@ -19,13 +19,21 @@ public class Day01 : BaseDay
             {
                 if (!string.IsNullOrWhiteSpace(calorie)) blockCalories += int.Parse(calorie);
             }
-            if (blockCalories > maxCalories) maxCalories = blockCalories;
+            caloriesList.Add(blockCalories);
             blockCalories = 0;
         }
+        caloriesList.Sort();
+        caloriesList.Reverse();
+        int maxCalories = 0;
+        for (int i = 0; i < totalsToCount; i++)
+        {
+            maxCalories += caloriesList[i];
+        }
+        
         return maxCalories.ToString();
     }
 
-    public override ValueTask<string> Solve_1() => new(Part1());
+    public override ValueTask<string> Solve_1() => new(CountTotalCalories(1));
 
-    public override ValueTask<string> Solve_2() => new($"TBC");
+    public override ValueTask<string> Solve_2() => new(CountTotalCalories(3));
 }
