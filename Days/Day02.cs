ecce44441619b6bbd946b95ff13f30fce56faff9
James Pain <jamesepain@gmail.com>
2022-12-22 18:09:53 +0000

    Day02 WIP

--- Days/Day02.cs
+++ Days/Day02.cs
@@ -9,7 +9,63 @@ public class Day02 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"There's nothing here");
+    /*
+    A - Rock        X - Rock
+    B - Paper       Y - Paper
+    C - Sissors     Z - Sissors
+    */
 
-    public override ValueTask<string> Solve_2() => new($"There's nothing here");
+    private string Process()
+    {
+
+        return "Nothing here yet";
+    }
+
+    private bool didWin(char call, char response)
+    {
+
+        return false;
+    }
+
+    public override ValueTask<string> Solve_1() => new(Process());
+
+    public override ValueTask<string> Solve_2() => new($"I'm just a string");
+}
+
+class RPSGame
+{
+    (TheirChoice, MyChoice) roundChoices;
+
+    public bool didWin
+    {
+        get 
+        {
+            if (roundChoices.Item1 == roundChoices.Item2) {
+
+            }
+            return true;
+        }
+        set { return; }
+    }
+    
+
+    public RPSGame(char theirChoice, char myChoice){
+        roundChoices = ((TheirChoice)theirChoice, (MyChoice)myChoice);
+    }
+
+    enum TheirChoice
+    {
+        Rock = 'A',
+        Papper = 'B',
+        Sissors = 'C'
+    }
+    enum MyChoice
+    {
+        Rock = 'X',
+        Papper = 'Y',
+        Sissors = 'Z'
+    }
+
+    
+
 }
