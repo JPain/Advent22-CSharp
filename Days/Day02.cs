ae3d258fff74a5e0920ca6f0c2044de307962f1a
James Pain <james@thinkbit.uk>
2022-12-23 01:51:37 +0000

    Day 2 solved

--- Days/Day02.cs
+++ Days/Day02.cs
@@ -9,63 +9,52 @@ public class Day02 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    /*
-    A - Rock        X - Rock
-    B - Paper       Y - Paper
-    C - Sissors     Z - Sissors
-    */
-
-    private string Process()
+    private string Part1()
     {
-
-        return "Nothing here yet";
-    }
-
-    private bool didWin(char call, char response)
-    {
-
-        return false;
-    }
-
-    public override ValueTask<string> Solve_1() => new(Process());
-
-    public override ValueTask<string> Solve_2() => new($"I'm just a string");
-}
-
-class RPSGame
-{
-    (TheirChoice, MyChoice) roundChoices;
-
-    public bool didWin
-    {
-        get 
+        Dictionary<(char, char), int> answers = new Dictionary<(char, char), int>(){
+            {('A', 'X'), 4}, //rock rock 1 = draw 3
+            {('A', 'Y'), 8}, //rock paper 2 = win 6
+            {('A', 'Z'), 3}, //rock sissors 3 = lose 0
+            {('B', 'X'), 1}, //paper rock 1 = lose 0
+            {('B', 'Y'), 5}, //paper paper 2 = draw 3
+            {('B', 'Z'), 9}, //paper sissors 3 = win 6
+            {('C', 'X'), 7}, //sissors rock 1 = win 6
+            {('C', 'Y'), 2}, //sissors paper 2 = lose 0
+            {('C', 'Z'), 6}, //sissors sissors 3 = draw 3
+        };
+        int score = 0;
+        foreach (string line in _input.Split(Environment.NewLine))
         {
-            if (roundChoices.Item1 == roundChoices.Item2) {
-
-            }
-            return true;
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            score += answers[(line[0],line[2])];
         }
-        set { return; }
-    }
-    
-
-    public RPSGame(char theirChoice, char myChoice){
-        roundChoices = ((TheirChoice)theirChoice, (MyChoice)myChoice);
+        return score.ToString();
     }
 
-    enum TheirChoice
+    private string Part2()
     {
-        Rock = 'A',
-        Papper = 'B',
-        Sissors = 'C'
-    }
-    enum MyChoice
-    {
-        Rock = 'X',
-        Papper = 'Y',
-        Sissors = 'Z'
+        Dictionary<(char, char), int> answers = new Dictionary<(char, char), int>(){
+            {('A', 'X'), 3}, //rock sissors 3 = lose 0
+            {('A', 'Y'), 4}, //rock rock 1 = draw 3
+            {('A', 'Z'), 8}, //rock paper 2 = win 6
+            {('B', 'X'), 1}, //paper rock 1 = lose 0
+            {('B', 'Y'), 5}, //paper paper 2 = draw 3
+            {('B', 'Z'), 9}, //paper sissors 3 = win 6
+            {('C', 'X'), 2}, //sissors paper 2 = lose 0
+            {('C', 'Y'), 6}, //sissors sissors 3 = draw 3
+            {('C', 'Z'), 7}, //sissors rock 1 = win 6
+        };
+        int score = 0;
+        foreach (string line in _input.Split(Environment.NewLine))
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            score += answers[(line[0],line[2])];
+        }
+        return score.ToString();
     }
 
-    
+    public override ValueTask<string> Solve_1() => new(Part1());
 
+    public override ValueTask<string> Solve_2() => new(Part2());
 }
+
